31a7da4d099b6df03aa7012c95fcd52746695a31
Tomek Sułkowski <t.sulkowski@sages.com.pl>
2014-09-19 09:54:58 +0200

    localStorage caching

--- app/scripts/movies/movies-controller.js
+++ app/scripts/movies/movies-controller.js
@@ -4,10 +4,16 @@ angular.module('moviesApp.movies')
 
 .controller('MoviesCtrl', function ($scope, moviesService) {
 
+    $scope.query = moviesService.lastQuery();
+
     $scope.search = function (query) {
         moviesService.search(query).then(function (movies) {
             $scope.movies = movies;
         });
     };
+
+    if ($scope.query) {
+        $scope.search($scope.query);
+    }
 
 });
--- app/scripts/movies/movies-service.js
+++ app/scripts/movies/movies-service.js
@@ -7,12 +7,34 @@ angular.module('moviesApp.movies')
  * API docs: http://docs.themoviedb.apiary.io/
  */
 
-.factory('moviesService', function ($http, APIKEY) {
+.factory('moviesService', function ($http, $window, $q, APIKEY) {
 
     var apiUrl = 'http://api.themoviedb.org/3/search/movie';
 
     return {
+
+        lastQuery: function () {
+            return $window.localStorage.query;
+        },
+
         search: function (query) {
+            var deferred = $q.defer();
+
+            if (!query) {
+                deferred.resolve([]);
+                return deferred.promise;
+            }
+
+            if ($window.localStorage.query === query && $window.localStorage.movies) {    
+                
+                var movies = JSON.parse($window.localStorage.movies);
+                deferred.resolve(movies);
+                
+                return deferred.promise;
+            }
+
+            $window.localStorage.query = query;
+
             var config = {
                 params: {
                     query: query,
@@ -20,6 +42,7 @@ angular.module('moviesApp.movies')
                 }
             };
             return $http.get(apiUrl, config).then(function (response) {
+                $window.localStorage.movies = JSON.stringify(response.data.results);
                 return response.data.results;
             });
         }
