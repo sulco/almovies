b8c0dbf74797cadbcee75b1aa1a0fa2cf77dc382
Tomek Sułkowski <t.sulkowski@sages.com.pl>
2014-09-19 13:37:28 +0200

    unit tests task

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')();
 
+var karma = require('karma').server;
+
 var wiredep = require('wiredep');
 
 gulp.task('test', function() {
@@ -15,8 +17,10 @@ gulp.task('test', function() {
   });
 
   var testFiles = bowerDeps.js.concat([
-    'app/scripts/**/*.js',
-    'test/unit/**/*.js'
+    'app/scripts/moviesApp.js',
+    'app/scripts/movies/movies.js',
+    'app/scripts/bookmarks/bookmarks.js',
+    'app/scripts/**/*.js'
   ]);
 
   return gulp.src(testFiles)
@@ -28,4 +32,33 @@ gulp.task('test', function() {
       // Make sure failed tests cause gulp to exit non-zero
       throw err;
     });
+});
+
+
+gulp.task('tdd', function (done) {
+
+  var bowerDeps = wiredep({
+    directory: 'app/bower_components',
+    exclude: ['bootstrap-sass-official'],
+    dependencies: true,
+    devDependencies: true
+  });
+
+  var testFiles = bowerDeps.js.concat([
+    'app/scripts/moviesApp.js',
+    'app/scripts/movies/movies.js',
+    'app/scripts/bookmarks/bookmarks.js',
+    'app/scripts/**/*.js'
+  ]);
+
+  karma.start({
+    files : testFiles,
+    autoWatch : true,
+    frameworks: ['jasmine'],
+    browsers : ['PhantomJS'],
+    plugins : [
+        'karma-phantomjs-launcher',
+        'karma-jasmine'
+    ]
+  }, done);
 });
